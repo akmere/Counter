8c80b1310ebb30627abe61603a2263b7e4edf71c
akmere <rafkam0@gmail.com>
2017-09-04 02:19:43 +0200

    Initial

---
+++ Counter/Counter/Counter/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Counter
+{
+    public partial class App : Application
+    {
+        Label CountingLabel, CurrentLabel, StartLabel;
+        Button ResetButton;
+        DateTime CurrentTime, SavedTime;
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new Counter.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            SavedTime = System.DateTime.UtcNow;
+            CurrentTime = System.DateTime.Now;
+            CountingLabel = MainPage.FindByName<Label>("label1");
+            CountingLabel.Text = (CurrentTime - SavedTime).ToString();
+            CurrentLabel = MainPage.FindByName<Label>("label2");
+            CurrentLabel.Text = "Current Time: " + CurrentTime.ToString();
+            StartLabel = MainPage.FindByName<Label>("label3");
+            StartLabel.Text = "Initial Time: " + SavedTime.ToString();
+            ResetButton = MainPage.FindByName<Button>("button1");
+            ResetButton.Clicked += delegate { Lol(); };
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+        public void Lol()
+        {
+            MainPage.DisplayAlert("Hello", "LOL", "Accept");
+        }
+    }
+}
---
+++ Counter/Counter/Counter/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Counter
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
